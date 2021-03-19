ea817f4f0dc88482a5377931b0e35416813c59f5
tiago19fp <56730393+tiago19fp@users.noreply.github.com>
2021-03-19 12:19:03 +0000

    Add files via upload

--- concentrador/concentrador.c
+++ concentrador/concentrador.c
@@ -7,26 +7,26 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <time.h>
 
-#define SetBit(A,k)     ( A[(k/8)] |= (1 << (k%8)) )
-#define ClearBit(A,k)   ( A[(k/8)] &= ~(1 << (k%8)) )
-#define TestBit(A,k)    ( A[(k/8)] & (1 << (k%8)) )
+#define SetBit(A, k) (A[(k / 8)] |= (1 << (k % 8)))
+#define ClearBit(A, k) (A[(k / 8)] &= ~(1 << (k % 8)))
+#define TestBit(A, k) (A[(k / 8)] & (1 << (k % 8)))
 
 void print_bits(unsigned char x)
 {
-   int i;
-   for (i = 8 * sizeof(x) - 1; i >= 0; i--)
-   {
-      (x & (1 << i)) ? putchar('1') : putchar('0');
-   }
-   printf("\n");
+    int i;
+    for (i = 8 * sizeof(x) - 1; i >= 0; i--)
+    {
+        (x & (1 << i)) ? putchar('1') : putchar('0');
+    }
+    printf("\n");
 }
 
 int port;
 int sampleTime;
+int sampleFreq;
 char *typeCom;
-
-char startPacket[3] = {0};
 
 int server_fd, new_socket, valread;
 struct sockaddr_in address;
@@ -55,11 +55,13 @@ void getConfigFile()
         }
         port = atoi(arguments[0]);
         sampleTime = atoi(arguments[1]);
-        typeCom = arguments[2];
+        sampleFreq = atoi(arguments[2]);
+        typeCom = arguments[3];
     }
 }
 
-void initSocket(){
+void initSocket()
+{
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -88,51 +90,72 @@ void initSocket(){
     }
 }
 
-char* creatStartPacket(){
-    SetBit(startPacket, 8);
-    printf("%d\n", sampleTime);
-    startPacket[2] = sampleTime;  
+void creatStartPacket(char *packet, int sampleTime, int sampleFreq)
+{
+    packet[0] = (char)0;
+    packet[1] = (char)1;
+    packet[2] = (char)sampleTime;
+    packet[3] = (char)sampleFreq;
+}
+
+void creatStopPacket(char *packet, int systemId, char stopReason)
+{
+
+    packet[0] = (char)1;
+    packet[1] = (char)systemId;
+    packet[2] = stopReason;
+}
+
+void creatErrorPacket(char *packet, int systemId, char errorType)
+{
+    packet[0] = (char)3;
+    packet[1] = (char)systemId;
+    packet[2] = errorType;
 }
 
 int main(int argc, char const *argv[])
 {
     char buffer[1024] = {0};
-    char *hello = "Boas mpt";
-    char startArray[3] = {0};
+    char *startPacket;
+    char *stopPacket;
+    char *errrotPacket;
+
+    time_t timeStamp;
+    time(&timeStamp);
+    printf("\n current time is : %s", ctime(&timeStamp));
 
     getConfigFile();
-    
-    int logError = open("logError.csv", O_CREAT | O_RDWR , 0600);
-    int logSamples = open("logSamples.csv", O_CREAT | O_RDWR , 0600);
-    int logState = open("logState.csv", O_CREAT | O_RDWR , 0600);
 
-    printf("Port: %d\nSample Time: %d\nType Com: %s\n", port, sampleTime, typeCom);
-    creatStartPacket();
+    int logError = open("logError.csv", O_CREAT | O_RDWR, 0600);
+    int logSamples = open("logSamples.csv", O_CREAT | O_RDWR, 0600);
+    int logState = open("logState.csv", O_CREAT | O_RDWR, 0600);
+
+    printf("Port: %d\nSample Time: %d\nSample Freq: %d\nType Com: %s\n", port, sampleTime, sampleFreq, typeCom);
+    creatStartPacket(startPacket, sampleTime, sampleFreq);
+    creatStopPacket(stopPacket, 2, 'A');
+    creatErrorPacket(errrotPacket, 3, 'B');
+
     for (int i = 0; i < 3; i++)
     {
-        print_bits(startPacket[i]);
+        print_bits(errrotPacket[i]);
     }
-
     initSocket();
-    
-    while(1){
 
-    if (listen(server_fd, 3) < 0)
+    while (1)
     {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                             (socklen_t *)&addrlen)) < 0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                                 (socklen_t *)&addrlen)) < 0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
 
-    //valread = read(new_socket, buffer, 1024);
-    printf("Mensagem recebida: %s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    //memset(buffer, '\0' , sizeof(buffer));
+        send(new_socket, errrotPacket, sizeof(char) * 3, 0);
     }
     return 0;
 }
